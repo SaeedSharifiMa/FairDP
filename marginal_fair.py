a360c7780cd6a68b2c0323663a18112c3a286a21
Saeed Sharifi <33630649+SaeedSharifiMa@users.noreply.github.com>
2018-12-22 10:33:41 -0500

    Update marginal_fair.py

--- marginal_fair.py
+++ marginal_fair.py
@@ -419,7 +419,7 @@ data_list = ['student', 'communities', 'adult', 'lawschool']
 
 # this is the original list of gamma values
 base_gamma_list = [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008,
-            0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.08, 0.09,
+            0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
             0.1, 0.2, 0.4, 0.8, 1]
 gamma_list = sorted(set(base_gamma_list + list(np.linspace(0.01, 0.2, 51))))
 
